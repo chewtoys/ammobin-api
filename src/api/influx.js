a5855cdcb06d53a648ff245787a71416385b4e13
Simon Ramsay <nexus-uw@users.noreply.github.com>
2018-01-22 17:27:08 -0500

    progress
    - moved everything into own dbs
    - moved all fields into tags
    - created 7d retention
    
    todo
    downsample to 24h
    > CREATE CONTINUOUS QUERY "cq_1d" ON "X" BEGIN
      SELECT count("c") AS "count"
        INTO "a_year"."downsampled_X"
          FROM "X"
            GROUP BY time(1d),
    	GROUP BY TAGS...
    	END

--- src/api/influx.js
+++ src/api/influx.js
@@ -6,21 +6,26 @@ const influxClicksDb = new Influx.InfluxDB({
   schema: [
     {
       measurement: 'click',
-      fields: {
-        url: Influx.FieldType.STRING,
-        cost: Influx.FieldType.FLOAT,
-        unitCost: Influx.FieldType.FLOAT,
-        count: Influx.FieldType.INTEGER,
-        name: Influx.FieldType.STRING,
-      },
+      fields: { v: Influx.FieldType.BOOLEAN },
       tags: [
         'userAgent',
         'vendor',
         'province',
         'brand',
         'calibre',
+        'url',
+        'cost',
+        'unitCost',
+        'count',
+        'name',
       ]
     },
+  ]
+})
+const influxViewsDb = new Influx.InfluxDB({
+  host: 'influx',
+  database: 'views',
+  schema: [
     {
       measurement: 'view',
       fields: { v: Influx.FieldType.BOOLEAN },
@@ -30,38 +35,47 @@ const influxClicksDb = new Influx.InfluxDB({
         'calibre',
       ]
     },
+  ]
+})
+const influxItemsDb = new Influx.InfluxDB({
+  host: 'influx',
+  database: 'items',
+  schema: [
     {
       measurement: 'item',
-      fields: {
-        name: Influx.FieldType.STRING,
-        price: Influx.FieldType.FLOAT,
-        unitCost: Influx.FieldType.FLOAT,
-      },
+      fields: { v: Influx.FieldType.BOOLEAN },
       tags: [
         'vendor',
         'province',
         'brand',
         'calibre',
+        'name',
+        'price',
+        'unitCost',
       ]
-    },
+    }
+  ]
+})
+const influxScrapesDb = new Influx.InfluxDB({
+  host: 'influx',
+  database: 'scrapes',
+  schema: [
     {
       measurement: 'scrape',
-      fields: {
-        results: Influx.FieldType.INTEGER,
-        time: Influx.FieldType.INTEGER,
-      },
+      fields: { v: Influx.FieldType.BOOLEAN },
       tags: [
+        'results',
+        'time',
         'vendor',
         'type',
       ]
     },
     {
       measurement: 'scrape-failure',
-      fields: {
-        time: Influx.FieldType.INTEGER,
-        error: Influx.FieldType.STRING
-      },
+      fields: { v: Influx.FieldType.BOOLEAN },
       tags: [
+        'time',
+        'error',
         'vendor',
         'type',
       ]
@@ -69,116 +83,120 @@ const influxClicksDb = new Influx.InfluxDB({
   ]
 });
 
-const prom = influxClicksDb.getDatabaseNames()
-  .then(names => {
-    if (!names.includes('clicks')) {
-      return influxClicksDb.createDatabase('clicks');
-    }
-  })
-  .catch(e => {
-    console.error('FAILED to connect to influx', e);
-    throw e;
-  });
+const retention = '7d'
+const prom = Promise.all([
+  [influxClicksDb, 'clicks'],
+  [influxViewsDb, 'views'],
+  [influxScrapesDb, 'scrapes'],
+  [influxItemsDb, 'items'],
+].map(async k => {
+  // get dbs, and create if not found
+  const names = await k[0].getDatabaseNames()
+  if (!names.includes(k[1])) {
+    await k[0].createDatabase(k[1])
+  }
 
+  // get policies, if not found create, else update
+  const policies = (await k[0].showRetentionPolicies(k[1])).map(p => p.name)
+  if (!policies.includes(retention)) {
+    await k[0].createRetentionPolicy(retention, { duration: retention, replication: 1 })
+  } else {
+    await k[0].alterRetentionPolicy(retention, {
+      duration: retention,
+      replication: 1,
+      default: true
+    })
+  }
+
+}))
 
 module.exports = {
-  logClick(url, userAgent, item) {
-    return prom
-      .then(() =>
-        influxClicksDb.writePoints([
-          {
-            measurement: 'clicks',
-            tags: {
-              userAgent,
-              vendor: item.vendor,
-              province: item.province,
-              brand: item.brand,
-              calibre: item.calibre
-            },
-            fields: {
-              url,
-              price: item.price || 0,
-              unitCost: item.unitCost || 0,
-              count: item.count || 0,
-              name: item.name,
-            }
-          }
-        ])
-      )
+  async logClick(url, userAgent, item) {
+    await prom
+    await influxClicksDb.writePoints([
+      {
+        measurement: 'clicks',
+        tags: {
+          userAgent,
+          vendor: item.vendor,
+          province: item.province,
+          brand: item.brand,
+          calibre: item.calibre,
+          url,
+          price: item.price || 0,
+          unitCost: item.unitCost || 0,
+          count: item.count || 0,
+          name: item.name,
+        },
+        fields: { v: true }
+      }
+    ])
+
   },
-  logView(userAgent, brand, calibre) {
-    return prom
-      .then(() =>
-        influxClicksDb.writePoints([
-          {
-            measurement: 'view',
-            tags: {
-              userAgent,
-              brand,
-              calibre
-            },
-            fields: { v: true }
-          }
-        ])
-      )
+  async logView(userAgent, brand, calibre) {
+    await prom
+    await influxViewsDb.writePoints([
+      {
+        measurement: 'view',
+        tags: {
+          userAgent,
+          brand,
+          calibre
+        },
+        fields: { v: true }
+      }
+    ])
+
   },
-  logItem(item) {
-    return prom
-      .then(() =>
-        influxClicksDb.writePoints([
-          {
-            measurement: 'item',
-            tags: {
-              vendor: item.vendor,
-              province: item.province,
-              brand: item.brand,
-              calibre: item.calibre
-            },
-            fields: {
-              price: item.price || 0,
-              unitCost: item.unitCost || null,
-              name: item.name,
-            }
-          }
-        ])
-      )
+  async logItem(item) {
+    await prom
+    await influxItemsDb.writePoints([
+      {
+        measurement: 'item',
+        tags: {
+          vendor: item.vendor,
+          province: item.province,
+          brand: item.brand,
+          calibre: item.calibre,
+          price: item.price || 0,
+          unitCost: item.unitCost || null,
+          name: item.name,
+        },
+        fields: { v: true }
+      }
+    ])
+
   },
-  logScrapeResult(type, vendor, results, time) {
-    return prom
-      .then(() =>
-        influxClicksDb.writePoints([
-          {
-            measurement: 'scrape',
-            tags: {
-              vendor,
-              type
-            },
-            fields: {
-              results,
-              time
-            }
-          }
-        ])
-      )
+  async logScrapeResult(type, vendor, results, time) {
+    await prom
+    await influxScrapesDb.writePoints([
+      {
+        measurement: 'scrape',
+        tags: {
+          vendor,
+          type,
+          results,
+          time
+        },
+        fields: { v: true }
+      }
+    ])
+
   },
-  logScrapeFail(type, vendor, time, error) {
-    return prom
-      .then(() =>
-        influxClicksDb.writePoints([
-          {
-            measurement: 'scrape-failure',
-            tags: {
-              vendor,
-              type
-            },
-            fields: {
-              time,
-              error
-            }
-          }
-        ])
-      )
-  },
-  // tmp. remove once redis no longer sole db
-  influxClicksDb
+  async logScrapeFail(type, vendor, time, error) {
+    await prom
+    await influxScrapesDb.writePoints([
+      {
+        measurement: 'scrape-failure',
+        tags: {
+          vendor,
+          type,
+          time,
+          error
+        },
+        fields: { v: true }
+      }
+    ])
+
+  }
 }
